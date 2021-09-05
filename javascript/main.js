bfa06ab290ed556cafde62dc08e8273690f9455a
Swiatlon <wiercikdoc@gmail.com>
2021-09-05 20:54:18 +0200

    Downloaded html elements for changingColor.And added the functioning of changing colors

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,15 @@ const spinner =document.querySelector('.spinner');
 const speedUp= document.querySelector('.speedUp');
 const speedDown= document.querySelector('.speedDown');
 let speed = 10; 
+//There I downloaded all elements that will change color with interaction
+const firstAmongus = [...document.querySelectorAll('.firstAmongus')];
+const firstAmongusInput = document.querySelector('.firstAmongusInput');
+const secondAmongus = [...document.querySelectorAll('.secondAmongus')];
+const secondAmongusInput = document.querySelector('.secondAmongusInput');
+const thirdAmongus = [...document.querySelectorAll('.thirdAmongus')];
+const thirdAmongusInput = document.querySelector('.thirdAmongusInput');
+const colorSpinner = [...document.querySelectorAll('.colorSpinner')];
+const colorSpinnerInput = document.querySelector('.colorSpinnerInput');
 
 const spinningAnimation = function(){
     spinButton.disabled = true;
@@ -33,7 +42,19 @@ const changingSpeed = function(modifier){
     const number =eval(modifier);
     speed = speed + number
     console.log(speed);
-}   
+}
+
+const changingColorOfItem = function(event,array){
+    if(event.target.value.length > 2){
+        array.forEach(item =>item.attributes.fill.value = `${event.target.value}`);
+    }
+}
+
+firstAmongusInput.addEventListener('focusout',function(){changingColorOfItem(event,firstAmongus)});
+secondAmongusInput.addEventListener('focusout',function(){changingColorOfItem(event,secondAmongus)});
+thirdAmongusInput.addEventListener('focusout',function(){changingColorOfItem(event,thirdAmongus)});
+colorSpinnerInput.addEventListener('focusout',function(){changingColorOfItem(event,colorSpinner)});
+
 spinButton.addEventListener("click",spinningAnimation);
 speedUp.addEventListener('click', function(){changingSpeed("+1")});
 speedDown.addEventListener('click',function(){changingSpeed("-1")});
